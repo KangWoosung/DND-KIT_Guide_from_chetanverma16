97545cbdf30b07950aa08cf279ea64e4968e9ea9
KangWoosung <53413020+KangWoosung@users.noreply.github.com>
2024-02-24 00:07:51 +0900

    InitData Added..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 // DnD
 import {
@@ -14,40 +14,85 @@ import {
   closestCorners,
   useSensor,
   useSensors,
-} from '@dnd-kit/core';
+} from "@dnd-kit/core";
 import {
   SortableContext,
   arrayMove,
   sortableKeyboardCoordinates,
-} from '@dnd-kit/sortable';
+} from "@dnd-kit/sortable";
 
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 
 // Components
-import Container from '@/components/Container';
-import Items from '@/components/Item';
-import Modal from '@/components/Modal';
-import Input from '@/components/Input';
-import { Button } from '@/components/Button';
+import Container from "@/components/Container";
+import Items from "@/components/Item";
+import Modal from "@/components/Modal";
+import Input from "@/components/Input";
+import { Button } from "@/components/Button";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 type DNDType = {
   id: UniqueIdentifier;
   title: string;
-  items: {
-    id: UniqueIdentifier;
-    title: string;
-  }[];
+  items?: DNDItemType[];
+};
+type DNDItemType = {
+  id: UniqueIdentifier;
+  title: string;
 };
 
+// console.log(crypto.randomUUID());
+// console.log(crypto.randomUUID());
+
+const initContainerData = [
+  {
+    id: `container-${uuidv4()}`,
+    title: "Container 1",
+    items: [
+      {
+        id: `item-${uuidv4()}`,
+        title: "Item 1",
+      },
+      {
+        id: `item-${uuidv4()}`,
+        title: "Item 2",
+      },
+    ],
+  },
+  {
+    id: `container-${uuidv4()}`,
+    title: "Container 2",
+    items: [
+      {
+        id: `item-${uuidv4()}`,
+        title: "Item 3",
+      },
+      {
+        id: `item-${uuidv4()}`,
+        title: "Item 4",
+      },
+    ],
+  },
+  {
+    id: `container-${uuidv4()}`,
+    title: "Container 3",
+    items: [
+      {
+        id: `item-${uuidv4()}`,
+        title: "Item 6",
+      },
+    ],
+  },
+];
+
 export default function Home() {
-  const [containers, setContainers] = useState<DNDType[]>([]);
+  const [containers, setContainers] = useState<DNDType[]>(initContainerData);
   const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
   const [currentContainerId, setCurrentContainerId] =
     useState<UniqueIdentifier>();
-  const [containerName, setContainerName] = useState('');
-  const [itemName, setItemName] = useState('');
+  const [containerName, setContainerName] = useState("");
+  const [itemName, setItemName] = useState("");
   const [showAddContainerModal, setShowAddContainerModal] = useState(false);
   const [showAddItemModal, setShowAddItemModal] = useState(false);
 
@@ -62,7 +107,7 @@ export default function Home() {
         items: [],
       },
     ]);
-    setContainerName('');
+    setContainerName("");
     setShowAddContainerModal(false);
   };
 
@@ -70,44 +115,45 @@ export default function Home() {
     if (!itemName) return;
     const id = `item-${uuidv4()}`;
     const container = containers.find((item) => item.id === currentContainerId);
-    if (!container) return;
+    if (!container || !container.items) return;
     container.items.push({
       id,
       title: itemName,
     });
     setContainers([...containers]);
-    setItemName('');
+    setItemName("");
     setShowAddItemModal(false);
   };
 
   // Find the value of the items
   function findValueOfItems(id: UniqueIdentifier | undefined, type: string) {
-    if (type === 'container') {
+    if (type === "container") {
       return containers.find((item) => item.id === id);
     }
-    if (type === 'item') {
+    if (type === "item") {
+      if (!containers) return;
       return containers.find((container) =>
-        container.items.find((item) => item.id === id),
+        container.items?.find((item) => item.id === id)
       );
     }
   }
 
   const findItemTitle = (id: UniqueIdentifier | undefined) => {
-    const container = findValueOfItems(id, 'item');
-    if (!container) return '';
-    const item = container.items.find((item) => item.id === id);
-    if (!item) return '';
+    const container = findValueOfItems(id, "item");
+    if (!container) return "";
+    const item = container.items?.find((item) => item.id === id);
+    if (!item) return "";
     return item.title;
   };
 
   const findContainerTitle = (id: UniqueIdentifier | undefined) => {
-    const container = findValueOfItems(id, 'container');
-    if (!container) return '';
+    const container = findValueOfItems(id, "container");
+    if (!container) return "";
     return container.title;
   };
 
   const findContainerItems = (id: UniqueIdentifier | undefined) => {
-    const container = findValueOfItems(id, 'container');
+    const container = findValueOfItems(id, "container");
     if (!container) return [];
     return container.items;
   };
@@ -117,7 +163,7 @@ export default function Home() {
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
-    }),
+    })
   );
 
   function handleDragStart(event: DragStartEvent) {
@@ -131,96 +177,106 @@ export default function Home() {
 
     // Handle Items Sorting
     if (
-      active.id.toString().includes('item') &&
-      over?.id.toString().includes('item') &&
+      active.id.toString().includes("item") &&
+      over?.id.toString().includes("item") &&
       active &&
       over &&
       active.id !== over.id
     ) {
       // Find the active container and over container
-      const activeContainer = findValueOfItems(active.id, 'item');
-      const overContainer = findValueOfItems(over.id, 'item');
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "item");
 
       // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
 
       // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
-        (container) => container.id === activeContainer.id,
+        (container) => container.id === activeContainer.id
       );
       const overContainerIndex = containers.findIndex(
-        (container) => container.id === overContainer.id,
+        (container) => container.id === overContainer.id
       );
 
       // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id,
+      const activeitemIndex = activeContainer.items?.findIndex(
+        (item) => item.id === active.id
       );
-      const overitemIndex = overContainer.items.findIndex(
-        (item) => item.id === over.id,
+      const overitemIndex = overContainer.items?.findIndex(
+        (item) => item.id === over.id
       );
       // In the same container
       if (activeContainerIndex === overContainerIndex) {
         let newItems = [...containers];
-        newItems[activeContainerIndex].items = arrayMove(
-          newItems[activeContainerIndex].items,
-          activeitemIndex,
-          overitemIndex,
-        );
+
+        if (newItems[activeContainerIndex].items !== undefined) {
+          newItems[activeContainerIndex].items = arrayMove(
+            newItems[activeContainerIndex].items as DNDItemType[], // 타입 단언
+            activeitemIndex as number,
+            overitemIndex as number
+          );
+        }
 
         setContainers(newItems);
       } else {
         // In different containers
         let newItems = [...containers];
-        const [removeditem] = newItems[activeContainerIndex].items.splice(
-          activeitemIndex,
-          1,
-        );
-        newItems[overContainerIndex].items.splice(
-          overitemIndex,
-          0,
-          removeditem,
-        );
-        setContainers(newItems);
+        if (
+          newItems[activeContainerIndex].items !== undefined &&
+          newItems[overContainerIndex].items !== undefined
+        ) {
+          const removeditem = newItems[activeContainerIndex].items?.splice(
+            activeitemIndex as number,
+            1
+          );
+          newItems[overContainerIndex].items?.splice(
+            overitemIndex as number,
+            0,
+            ...(removeditem as DNDItemType[])
+          );
+          setContainers(newItems);
+        }
       }
     }
 
     // Handling Item Drop Into a Container
     if (
-      active.id.toString().includes('item') &&
-      over?.id.toString().includes('container') &&
+      active.id.toString().includes("item") &&
+      over?.id.toString().includes("container") &&
       active &&
       over &&
       active.id !== over.id
     ) {
       // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, 'item');
-      const overContainer = findValueOfItems(over.id, 'container');
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "container");
 
       // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
 
       // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
-        (container) => container.id === activeContainer.id,
+        (container) => container.id === activeContainer.id
       );
       const overContainerIndex = containers.findIndex(
-        (container) => container.id === overContainer.id,
+        (container) => container.id === overContainer.id
       );
 
       // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id,
+      const activeitemIndex = activeContainer.items?.findIndex(
+        (item) => item.id === active.id
       );
 
       // Remove the active item from the active container and add it to the over container
       let newItems = [...containers];
-      const [removeditem] = newItems[activeContainerIndex].items.splice(
-        activeitemIndex,
-        1,
+      const removeditem = newItems[activeContainerIndex].items?.splice(
+        activeitemIndex as number,
+        1
       );
-      newItems[overContainerIndex].items.push(removeditem);
-      setContainers(newItems);
+      if (removeditem !== undefined) {
+        newItems[overContainerIndex].items?.push(removeditem[0]);
+        setContainers(newItems);
+      }
     }
   };
 
@@ -230,18 +286,18 @@ export default function Home() {
 
     // Handling Container Sorting
     if (
-      active.id.toString().includes('container') &&
-      over?.id.toString().includes('container') &&
+      active.id.toString().includes("container") &&
+      over?.id.toString().includes("container") &&
       active &&
       over &&
       active.id !== over.id
     ) {
       // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
-        (container) => container.id === active.id,
+        (container) => container.id === active.id
       );
       const overContainerIndex = containers.findIndex(
-        (container) => container.id === over.id,
+        (container) => container.id === over.id
       );
       // Swap the active and over container
       let newItems = [...containers];
@@ -251,90 +307,92 @@ export default function Home() {
 
     // Handling item Sorting
     if (
-      active.id.toString().includes('item') &&
-      over?.id.toString().includes('item') &&
+      active.id.toString().includes("item") &&
+      over?.id.toString().includes("item") &&
       active &&
       over &&
       active.id !== over.id
     ) {
       // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, 'item');
-      const overContainer = findValueOfItems(over.id, 'item');
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "item");
 
       // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
       // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
-        (container) => container.id === activeContainer.id,
+        (container) => container.id === activeContainer.id
       );
       const overContainerIndex = containers.findIndex(
-        (container) => container.id === overContainer.id,
+        (container) => container.id === overContainer.id
       );
       // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id,
+      const activeitemIndex = activeContainer.items?.findIndex(
+        (item) => item.id === active.id
       );
-      const overitemIndex = overContainer.items.findIndex(
-        (item) => item.id === over.id,
+      const overitemIndex = overContainer.items?.findIndex(
+        (item) => item.id === over.id
       );
 
       // In the same container
       if (activeContainerIndex === overContainerIndex) {
         let newItems = [...containers];
         newItems[activeContainerIndex].items = arrayMove(
-          newItems[activeContainerIndex].items,
-          activeitemIndex,
-          overitemIndex,
+          newItems[activeContainerIndex].items as DNDItemType[],
+          activeitemIndex as number,
+          overitemIndex as number
         );
         setContainers(newItems);
       } else {
         // In different containers
         let newItems = [...containers];
-        const [removeditem] = newItems[activeContainerIndex].items.splice(
-          activeitemIndex,
-          1,
+        const removeditem = newItems[activeContainerIndex].items?.splice(
+          activeitemIndex as number,
+          1
         );
-        newItems[overContainerIndex].items.splice(
-          overitemIndex,
+        newItems[overContainerIndex].items?.splice(
+          overitemIndex as number,
           0,
-          removeditem,
+          ...(removeditem as DNDItemType[])
         );
         setContainers(newItems);
       }
     }
     // Handling item dropping into Container
     if (
-      active.id.toString().includes('item') &&
-      over?.id.toString().includes('container') &&
+      active.id.toString().includes("item") &&
+      over?.id.toString().includes("container") &&
       active &&
       over &&
       active.id !== over.id
     ) {
       // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, 'item');
-      const overContainer = findValueOfItems(over.id, 'container');
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "container");
 
       // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
       // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
-        (container) => container.id === activeContainer.id,
+        (container) => container.id === activeContainer.id
       );
       const overContainerIndex = containers.findIndex(
-        (container) => container.id === overContainer.id,
+        (container) => container.id === overContainer.id
       );
       // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id,
+      const activeitemIndex = activeContainer.items?.findIndex(
+        (item) => item.id === active.id
       );
 
       let newItems = [...containers];
-      const [removeditem] = newItems[activeContainerIndex].items.splice(
-        activeitemIndex,
-        1,
+      const removeditem = newItems[activeContainerIndex].items?.splice(
+        activeitemIndex as number,
+        1
       );
-      newItems[overContainerIndex].items.push(removeditem);
-      setContainers(newItems);
+      if (removeditem !== undefined) {
+        newItems[overContainerIndex].items?.push(removeditem[0]);
+        setContainers(newItems);
+      }
     }
     setActiveId(null);
   }
@@ -373,7 +431,9 @@ export default function Home() {
         </div>
       </Modal>
       <div className="flex items-center justify-between gap-y-2">
-        <h1 className="text-gray-800 text-3xl font-bold">Dnd-kit Guide</h1>
+        <h1 className="text-gray-800 text-3xl font-bold">
+          Dnd-kit Guide with Next.js
+        </h1>
         <Button onClick={() => setShowAddContainerModal(true)}>
           Add Container
         </Button>
@@ -398,9 +458,11 @@ export default function Home() {
                     setCurrentContainerId(container.id);
                   }}
                 >
-                  <SortableContext items={container.items.map((i) => i.id)}>
+                  <SortableContext
+                    items={container.items?.map((i) => i.id) || []}
+                  >
                     <div className="flex items-start flex-col gap-y-4">
-                      {container.items.map((i) => (
+                      {container.items?.map((i) => (
                         <Items title={i.title} id={i.id} key={i.id} />
                       ))}
                     </div>
@@ -410,13 +472,13 @@ export default function Home() {
             </SortableContext>
             <DragOverlay adjustScale={false}>
               {/* Drag Overlay For item Item */}
-              {activeId && activeId.toString().includes('item') && (
+              {activeId && activeId.toString().includes("item") && (
                 <Items id={activeId} title={findItemTitle(activeId)} />
               )}
               {/* Drag Overlay For Container */}
-              {activeId && activeId.toString().includes('container') && (
+              {activeId && activeId.toString().includes("container") && (
                 <Container id={activeId} title={findContainerTitle(activeId)}>
-                  {findContainerItems(activeId).map((i) => (
+                  {findContainerItems(activeId)?.map((i) => (
                     <Items key={i.id} title={i.title} id={i.id} />
                   ))}
                 </Container>
